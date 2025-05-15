fc0c352de63289eb8a8f64f1e58f540136352557
unce2025 <cargoexpress.un@gmail.com>
2025-05-15 22:39:12 +0800

    Update getTracking.js

--- functions/getTracking.js
+++ functions/getTracking.js
@@ -1,14 +1,17 @@
 async function getTracking(trackingNumber) {
   const url = `https://script.google.com/macros/s/AKfycbzrtpPr6-NoxotSQ-upinCohoElIJxQQO259cpYlUOjhSFLcIcng5m_MnDeh6r9tyR5/exec?tracking=${trackingNumber}`;
+
   try {
     const response = await fetch(url);
     const result = await response.json();
+
     if (result.success && result.shipment) {
       displayShipment(result.shipment);
     } else {
       alert("Tracking number not found.");
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Error fetching tracking info:", error);
+    alert("Error fetching tracking info.");
   }
 }
