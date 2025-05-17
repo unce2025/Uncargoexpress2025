87c8bb761813c8248aa5e7b691c807c3cb51f4a8
unce2025 <cargoexpress.un@gmail.com>
2025-05-17 11:40:40 +0800

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 exports.handler = async function (event) {
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbxo2vOQGjozgSVt2Q0G_WoLEWXafbfIwak--pGKa7OomIRmbAtX0sXuFrxUbOj71Qg/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbxQ8DHyqWZN_t7gMQtNohtrxipVlenlmQtpWioOz2HBYIPumpYWNHnl1XkQ9vS5VnKH/exec";
 
   // Handle CORS preflight request
   if (event.httpMethod === "OPTIONS") {
