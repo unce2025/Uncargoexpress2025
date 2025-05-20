a06ecad5f4ee6efd9926550cb9f74275b5190514
unce2025 <cargoexpress.un@gmail.com>
2025-05-21 01:45:41 +0800

    Update getTracking.js

--- functions/getTracking.js
+++ functions/getTracking.js
@@ -1,5 +1,5 @@
 async function getTracking(trackingNumber) {
-  const url = https://script.google.com/macros/s/AKfycbwu3fx5fNd95SFm-skPSQT9BkMQ3oE0tblji3uttfz6D9gScWP_iqEtmy-3haoJp1R3/exec?tracking=${encodeURIComponent(trackingNumber)};
+  const url = `https://script.google.com/macros/s/AKfycbwu3fx5fNd95SFm-skPSQT9BkMQ3oE0tblji3uttfz6D9gScWP_iqEtmy-3haoJp1R3/exec?tracking=${encodeURIComponent(trackingNumber)}`;
 
   try {
     const response = await fetch(url);
